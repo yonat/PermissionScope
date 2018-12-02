d7915d99b6837208a6794dc21d89001125ae7cac
Qian GuoQiang <gonefish@gmail.com>
2018-12-02 14:39:42 +0800

    Fix @objc inference problem

--- PermissionScope/PermissionScope.swift
+++ PermissionScope/PermissionScope.swift
@@ -338,7 +338,30 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
             button.setTitle("Allow \(type)".localized.uppercased(), for: .normal)
         }
         
-        button.addTarget(self, action: Selector("request\(type)"), for: .touchUpInside)
+        switch type {
+        case .contacts:
+            button.addTarget(self, action: #selector(requestContacts), for: .touchUpInside)
+        case .locationAlways:
+            button.addTarget(self, action: #selector(requestLocationAlways), for: .touchUpInside)
+        case .locationInUse:
+            button.addTarget(self, action: #selector(requestLocationInUse), for: .touchUpInside)
+        case .notifications:
+            button.addTarget(self, action: #selector(requestNotifications), for: .touchUpInside)
+        case .microphone:
+            button.addTarget(self, action: #selector(requestMicrophone), for: .touchUpInside)
+        case .camera:
+            button.addTarget(self, action: #selector(requestCamera), for: .touchUpInside)
+        case .photos:
+            button.addTarget(self, action: #selector(requestPhotos), for: .touchUpInside)
+        case .reminders:
+            button.addTarget(self, action: #selector(requestReminders), for: .touchUpInside)
+        case .events:
+            button.addTarget(self, action: #selector(requestEvents), for: .touchUpInside)
+        case .bluetooth:
+            button.addTarget(self, action: #selector(requestBluetooth), for: .touchUpInside)
+        case .motion:
+            button.addTarget(self, action: #selector(requestMotion), for: .touchUpInside)
+        }
 
         button.accessibilityIdentifier = "permissionscope.button.\(type)".lowercased()
         
@@ -419,7 +442,7 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
     /**
     Requests access to LocationAlways, if necessary.
     */
-    public func requestLocationAlways() {
+    @objc public func requestLocationAlways() {
     	let hasAlwaysKey:Bool = !Bundle.main
     		.object(forInfoDictionaryKey: Constants.InfoPlistKeys.locationAlways).isNil
     	assert(hasAlwaysKey, Constants.InfoPlistKeys.locationAlways + " not found in Info.plist.")
@@ -465,7 +488,7 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
     /**
     Requests access to LocationWhileInUse, if necessary.
     */
-    public func requestLocationInUse() {
+    @objc public func requestLocationInUse() {
     	let hasWhenInUseKey :Bool = !Bundle.main
     		.object(forInfoDictionaryKey: Constants.InfoPlistKeys.locationWhenInUse).isNil
     	assert(hasWhenInUseKey, Constants.InfoPlistKeys.locationWhenInUse + " not found in Info.plist.")
@@ -518,7 +541,7 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
     /**
     Requests access to Contacts, if necessary.
     */
-    public func requestContacts() {
+    @objc public func requestContacts() {
         let status = statusContacts()
         switch status {
         case .unknown:
@@ -628,7 +651,7 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
     /**
     Requests access to User Notifications, if necessary.
     */
-    public func requestNotifications() {
+    @objc public func requestNotifications() {
         let status = statusNotifications()
         switch status {
         case .unknown:
@@ -675,7 +698,7 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
     /**
     Requests access to the Microphone, if necessary.
     */
-    public func requestMicrophone() {
+    @objc public func requestMicrophone() {
         let status = statusMicrophone()
         switch status {
         case .unknown:
@@ -713,7 +736,7 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
     /**
     Requests access to the Camera, if necessary.
     */
-    public func requestCamera() {
+    @objc public func requestCamera() {
         let status = statusCamera()
         switch status {
         case .unknown:
@@ -752,7 +775,7 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
     /**
     Requests access to Photos, if necessary.
     */
-    public func requestPhotos() {
+    @objc public func requestPhotos() {
         let status = statusPhotos()
         switch status {
         case .unknown:
@@ -790,7 +813,7 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
     /**
     Requests access to Reminders, if necessary.
     */
-    public func requestReminders() {
+    @objc public func requestReminders() {
         let status = statusReminders()
         switch status {
         case .unknown:
@@ -827,7 +850,7 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
     /**
     Requests access to Events, if necessary.
     */
-    public func requestEvents() {
+    @objc public func requestEvents() {
         let status = statusEvents()
         switch status {
         case .unknown:
@@ -888,7 +911,7 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
     /**
     Requests access to Bluetooth, if necessary.
     */
-    public func requestBluetooth() {
+    @objc public func requestBluetooth() {
         let status = statusBluetooth()
         switch status {
         case .disabled:
@@ -933,7 +956,7 @@ typealias resultsForConfigClosure     = ([PermissionResult]) -> Void
     /**
     Requests access to Core Motion Activity, if necessary.
     */
-    public func requestMotion() {
+    @objc public func requestMotion() {
         let status = statusMotion()
         switch status {
         case .unauthorized:
